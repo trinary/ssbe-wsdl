dc9a526fb06043ac52afff6fdce4152f0147f969
Erik Cunningham <ewc@absolute-performance.com>
2009-05-22 12:50:55 -0600

    people not having libs in the right place is annoying

--- vendor/gems/addressable-2.0.2/init.rb
+++ vendor/gems/addressable-2.0.2/init.rb
@@ -1,7 +1,7 @@
 
 require_options = ["addressable"]
 if require_lib = require_options.find { |path|  File.directory?(File.join(File.dirname(__FILE__), 'lib', path)) }
-  require File.join(File.dirname(__FILE__), 'lib', require_lib)
+  require File.join(File.dirname(__FILE__), 'lib', require_lib,'uri')
 else
   puts msg = "ERROR: Please update #{File.expand_path __FILE__} with the require path for linked RubyGem addressable"
   exit
