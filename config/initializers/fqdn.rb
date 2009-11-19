147ea844ed54702dcba9d9c8e4742ba67dff9297
Erik Cunningham <ewc@absolute-awh.apiboulder.local>
2009-11-19 12:43:01 -0700

    Clean up credential init.

--- config/initializers/fqdn.rb
+++ config/initializers/fqdn.rb
@@ -5,10 +5,12 @@
 #
 CORE_FQDN=ENV['CORE_FQDN']
 ALARM_FQDN=ENV['ALARM_FQDN']
+SSBE_USER=ENV['SSBE_USER']
+SSBE_PASS=ENV['SSBE_PASS']
 
 DEFAULT_HTTP_ACCESSOR = CORE_HTTP_ACCESSOR = Resourceful::HttpAccessor.new(:logger => RAILS_DEFAULT_LOGGER,
                                                                            :cache_manager => Resourceful::InMemoryCacheManager.new)
 require 'system_shepherd/ssbe_authenticator'
-DEFAULT_HTTP_ACCESSOR.auth_manager.add_auth_handler(SSBEAuthenticator.new('dev', 'dev'))
+DEFAULT_HTTP_ACCESSOR.auth_manager.add_auth_handler(SSBEAuthenticator.new(SSBE_USER, SSBE_PASS))
 
 
