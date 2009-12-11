720b5c4003d44f5b57ae3552cfb501372a877ac1
Erik Cunningham <ewc@dhcp-125.boulder.api.local>
2009-12-11 11:52:52 -0700

    cleaner capfile, stylesheets dir so recipes work

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,37 +1,29 @@
 set :application, "rails-ws"
-set :repository,  "ssh://git.dev.api.local/git/ssbe/rails-ws.git"
-set :use_sudo, false
-set :user, 'root'
+set :repository,  "git://github.com/trinary/ssbe-wsdl.git"
 
-# load 'ext/rails-database-migrations.rb'
-# load 'ext/rails-shared-directories.rb'
-
-# load 'ext/spinner.rb'              # Designed for use with script/spin
-# load 'ext/passenger-mod-rails.rb'  # Restart task for use with mod_rails
-# load 'ext/web-disable-enable.rb'   # Gives you web:disable and web:enable
-
-# If you aren't deploying to /u/apps/#{application} on the target
-# servers (which is the default), you can specify the actual location
-# via the :deploy_to variable:
+set :scm, :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 set :deploy_to, "/opt/#{application}"
+set :user, "root"
+set :use_sudo, false
 
-# set :scm, "git"
-# see a full list by running "gem contents capistrano | grep 'scm/'"
+#role :web, "your web-server here"                          # Your HTTP server, Apache/etc
+#role :app, "qa-sssdp02-proc01.boulder.api.local"
+#role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
+#role :db,  "your slave db-server here"
+#
+server "qa-ssdp02-proc01.boulder.api.local", :app
 
-role :web, "172.18.0.15"
-role :app, "172.18.0.15"
+# If you are using Passenger mod_rails uncomment this:
+# if you're still using the script/reapear helper you will need
+# these http://github.com/rails/irs_process_scripts
 
 namespace :deploy do
-  task :start, :roles => :app do
-    run "touch #{current_release}/tmp/restart.txt"
-  end
-
-  task :stop, :roles => :app do
-    # Do nothing.
-  end
-
-  desc "Restart Application"
-  task :restart, :roles => :app do
-    run "touch #{current_release}/tmp/restart.txt"
-  end
-end
+   task :start do
+   end
+   task :stop do
+   end
+   task :restart, :roles => :app, :except => { :no_release => true } do
+     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+   end
+ end
